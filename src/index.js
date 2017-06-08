a84360082faa047493c38e8fcee66cb5f29a8e63
Lars Bolvig <larsbolvig@gmail.com>
2017-06-08 20:03:05 +0200

    update-login-logic - Move Provider from Router.js to index.js together with loginStore and Cognito setup

--- src/index.js
+++ src/index.js
@@ -2,4 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-redux';
+import ReduxThunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
+import {connect} from 'react-redux';
+import boliusLoginReducers from './Redux/reducers';
+import { cognitoInitialSetup } from './Redux/actions';
+import appConfig from './config';
+
+
+const loginStore = createStore(
+	boliusLoginReducers,
+	applyMiddleware(ReduxThunk)
+);
+
+// Initial Cognito setup
+loginStore.dispatch(cognitoInitialSetup({
+	region: appConfig.region,
+	UserPoolId: appConfig.UserPoolId,
+	ClientId: appConfig.ClientId
+}));
+
+ReactDOM.render(
+	<Provider store={loginStore}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
